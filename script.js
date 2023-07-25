53880e5c98a315ce2eedd0984ddf590c082e4945
MikeWentForth <themikewallace@gmail.com>
2023-07-25 09:56:31 -0700

    updated all files for weatherbox icons and functionality

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function startUp() {
 // store the entry to localstorage,
 // make the openweather API call,
 // and display the results.
-function citySearch(city="") {
+function citySearch(city = "") {
 
     // If no city provided in call, look to the input field...
     if (city == "") city = document.getElementById("cityInput").value;
@@ -54,7 +54,7 @@ function citySearch(city="") {
     // Call API
     //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
     var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`;
-    
+
     console.log(apiUrl);
     // fetch... then ... then...
     const req = new Request(apiUrl);
@@ -82,31 +82,66 @@ function citySearch(city="") {
             // hum -> data.list.0.main.humidity
             // wind -> data.list.0.wind.speed
 
+
+
             // convert dt to "human" date
             let weatherData = data["list"]["0"]; // shortcut helper
             let timestamp = parseInt(data["list"]["0"]["dt"]); // get timestamp from API data
-            let dateObj = new Date(timestamp*1000); // creating a javascript date object from the timestamp
+            let dateObj = new Date(timestamp * 1000); // creating a javascript date object from the timestamp
             // Multiplied by 1000 because the timestamp from the API is a UNIX system.
             // Javascript has to multiply a UNIX timstamp by 1000 to work correctly.
             // Use the date object to create a string with the format MM/DD/YYYY
-            let dateString = dateObj.getMonth()+1 + "/" + dateObj.getDate() + "/" + dateObj.getFullYear();
-            html = "<h2>" + data["city"]["name"] + " (" + dateString + ")</h2>";
-            html += "<p>" + timestamp + "</p>";
+            let dateString = dateObj.getMonth() + 1 + "/" + dateObj.getDate() + "/" + dateObj.getFullYear();
+            html = "<h2>" + data["city"]["name"] + " (" + dateString + ")";
+            let iconcode = weatherData['weather']['0']['icon'];
+            let iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
+            html += ` <img src="${iconurl}"></h2>`;
 
-            html += "<p>Humidity: " + weatherData.main.humidity + "</p>";
-    
+            let tempF = (1.8 * (parseFloat(weatherData.main.temp) - 273.15)) + 32;
+            tempF = tempF.toFixed(2);
+
+            html += "<p>Temperature: " + tempF + "&deg;F</p>";
+            html += "<p>Wind: " + weatherData.wind.speed + " MPH</p>";
+            html += "<p>Humidity: " + weatherData.main.humidity + " %</p>";
+
 
             // 5 DAY FORECAST
             // Iterate listnumber forward by 8s to get these
             // data.list.N......
             // Date, icon, temp, wind, humidity
+            let wind, humidity;
+            for (let i = 1; i <= 5; i++) {
+                let listIndex = (i * 8).toString();
+                weatherData = data['list'][listIndex];
+                timestamp = parseInt(weatherData.dt);
+                dateObj = new Date(timestamp * 1000);
+                dateString = dateObj.getMonth() + 1 + "/" + dateObj.getDate() + "/" + dateObj.getFullYear();
+                iconcode = weatherData['weather'][listIndex]['icon'];
+                iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
+                tempF = (1.8 * (parseFloat(weatherData.main.temp) - 273.15)) + 32;
+                tempF = tempF.toFixed(2);
+                wind = weatherData.wind.speed;
+                humidity = weatherData.main.humidity;
+
+                html = `<p>${dateString}</p>
+                        <p><img src="${iconurl}"></p>
+                        <p>Temperature: ${tempF}&deg;F</p>
+                        <p>Wind: ${wind} MPH</p>
+                        <p>Humidity: ${humidity} %</p>
+                `;
+
+                let divId = "forecastDiv" + (i.toString());
+
+                document.getElementById("divId").innerHTML = html;
+
+            }
 
 
 
 
             mainDiv.innerHTML = html;
 
-            
+
         })
         .catch(console.error);
 }
